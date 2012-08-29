b2fa07d91cfbb489dfe6cbf3130b4a658c2a3a22
Boris Shimanovsky <boris@factual.com>
2012-08-29 13:17:33 -0700

    adding hacky PUT support to allow us to measure uploaded document

--- lib/caliper_browser.rb
+++ lib/caliper_browser.rb
@@ -9,10 +9,29 @@ class CaliperBrowser
     Capybara.default_driver = :webkit
   end
     
-      
-  def get_measurements(url)
+  def get_page(url)
     visit(url)
     cram_in_jquery!
+  end
+  def load_string(str)
+    cram_in_jquery!
+    populate_script = <<-EOS
+      $('body').append($('<form id="hackedyHack"/>'));
+      $('#hackedyHack').append($('<textarea id="contentHolder" name="contentHolder"></textarea>'));
+    EOS
+    page.execute_script(populate_script)
+    within("#hackedyHack") do
+     fill_in('contentHolder', :with=>str)
+    end
+    replace_script = <<-EOS
+      $('html').html($('#contentHolder').val());
+    EOS
+    page.execute_script(replace_script)
+    cram_in_jquery!
+  end
+      
+  def get_measurements
+    apply_measure_scripts
     find('#calipermeasurements').text
   end
   
@@ -22,47 +41,20 @@ class CaliperBrowser
     begin
       
       jquery_add_script = <<-EOS
-      
-      function getXPath(elm) {
-       for (segs = []; elm && elm.nodeType == 1; elm = elm.parentNode) {
-         if (elm.hasAttribute('id')) {
-           segs.unshift('id("' + elm.getAttribute('id') + '")')
-           return segs.join('/')
-         }
-         else if (elm.hasAttribute('class'))
-           segs.unshift(elm.localName.toLowerCase() + '[@class="' + elm.getAttribute('class') + '"]')
-         else {
-           for (i = 1, sib = elm.previousSibling; sib; sib = sib.previousSibling)
-             if (sib.localName == elm.localName) i++
-           segs.unshift(elm.localName.toLowerCase() + '[' + i + ']')
-         }
-       }
-       return segs.length ? '/' + segs.join('/') : null
-      }
-
-      function doMeasurements(){
+      function addMeasurementContainer(){
         var $measurements = $('<div id="calipermeasurements"></div>');
         $('body').append($measurements);
-        var m = {};
-        $('*').each(function(idx,el){
-          var pos = $(el).position();
-          var w = $(el).width();
-          var h = $(el).height();
-          if (pos.left>0 && pos.top>0 && w>0 && h>0)
-            m[getXPath(el)] =  {x:pos.left, y:pos.top, w:w, h:h};
-         });
-        $measurements.html(JSON.stringify(m));
       }
 
       if (typeof jQuery != 'undefined') {
-        doMeasurements();
+        addMeasurementContainer();
       }
       else{
         var head = document.getElementsByTagName("head")[0];         
         var jqs = document.createElement('script');
         jqs.id = "fjquery";
         jqs.type = 'text/javascript';
-        jqs.onload=doMeasurements;
+        jqs.onload=addMeasurementContainer;
         jqs.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js';
         head.appendChild(jqs);
       }
@@ -71,12 +63,50 @@ class CaliperBrowser
       EOS
 
       page.execute_script(jquery_add_script)
-      page.has_css?('#calipermeasurements')
+      warn page.has_css?('#calipermeasurements')
       
     #rescue Capybara::Driver::Webkit::WebkitInvalidResponseError
     #  warn "FAILED!!"
     end
   end
+  
+  def apply_measure_scripts
+    measure_script = <<-EOS
+      function getXPath(elm) {
+        for (segs = []; elm && elm.nodeType == 1; elm = elm.parentNode) {
+          if (elm.hasAttribute('id')) {
+            segs.unshift('id("' + elm.getAttribute('id') + '")')
+            return segs.join('/')
+          }
+          else if (elm.hasAttribute('class'))
+            segs.unshift(elm.localName.toLowerCase() + '[@class="' + elm.getAttribute('class') + '"]')
+          else {
+            for (i = 1, sib = elm.previousSibling; sib; sib = sib.previousSibling)
+              if (sib.localName == elm.localName) i++
+            segs.unshift(elm.localName.toLowerCase() + '[' + i + ']')
+          }
+        }
+        return segs.length ? '/' + segs.join('/') : null
+        }
+
+        function doMeasurements(){
+          var m = {};
+          $('*').each(function(idx,el){
+            var pos = $(el).position();
+            var w = $(el).width();
+            var h = $(el).height();
+            if (pos.left>0 && pos.top>0 && w>0 && h>0)
+              m[getXPath(el)] =  {x:pos.left, y:pos.top, w:w, h:h};
+          });
+          $('#calipermeasurements').html(JSON.stringify(m));
+        }
+
+        doMeasurements();
+      EOS
+      
+      page.execute_script(measure_script)
+  end
+  
   
 
       
--- routes/measure.rb
+++ routes/measure.rb
@@ -1,8 +1,21 @@
 class App < Sinatra::Base
   $cp =  CaliperBrowser.new
+  
   get '/measure' do
-    content_type 'application/json'
     url = params[:url]
-    $cp.get_measurements(url)
+    $cp.get_page(url)
+    
+    content_type :json
+    $cp.get_measurements
+  end
+  
+  
+  put '/measure' do
+    html=request.body.read
+    $cp.load_string(html)
+    
+    
+    content_type :json
+    $cp.get_measurements
   end
 end
